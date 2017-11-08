a1d3780e7455c37b51738565698af881af9a509e
Benjamin Reinhold Bonnichsen <Benjamin@Benjamin-Reinhold-Bonnichsens-MacBook-Pro.local>
2017-11-08 07:33:00 +0100

    new

--- dokumentationssite/java.js
+++ dokumentationssite/java.js
@@ -1,23 +1,23 @@
-            //RETURN TO TOP BUTTON
-            window.onscroll = function () {
-                scrollFunction()
-            };
+$(document).ready(function () {
+    $(".container_drop").click(function () {
+        $(".container_drop").toggleClass("change");
+        $(".dropdown").slideToggle();
+    });
+});
+window.onscroll = function () {
+    scrollFunction()
+};
 
-            function scrollFunction() {
-                if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                    document.getElementById("myBtn").style.display = "block";
-                } else {
-                    document.getElementById("myBtn").style.display = "none";
-                }
-            }
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
 
-            function topFunction() {
-                document.body.scrollTop = 0; // For Chrome, Safari and Opera
-                document.documentElement.scrollTop = 0; // For IE and Firefox
-            }
-
-            // DROPDOWN MENU
-            $(".container_drop").click(function () {
-                $(".container_drop").toggleClass("change");
-                $(".dropdown").slideToggle();
-            });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
